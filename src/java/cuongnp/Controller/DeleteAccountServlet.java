073e212e1d3dc7fe0754a3fe35f1800c2f7125b5
cuongnguyenphu2005 <cuongnguyenphu2005@gmail.com>
2025-06-26 17:38:07 +0700

    Add delelte, update, cookie feature

---
+++ src/java/cuongnp/Controller/DeleteAccountServlet.java
@@ -0,0 +1,106 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package cuongnp.controller;
+
+import cuongnp.registration.RegistrationDAO;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Cuong
+ */
+@WebServlet(name = "DeleteAccountServlet", urlPatterns = {"/DeleteAccountServlet"})
+
+public class DeleteAccountServlet extends HttpServlet {
+
+    private final String ERROR_PAGE = "error.html";
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        //1. Get all user's information
+        String username = request.getParameter("pk");
+        String searchValue = request.getParameter("lastSearchValue");
+        String url = ERROR_PAGE;
+        try {
+            //2. Controller calls method Model
+            //2.1 New DAO object
+            RegistrationDAO dao = new RegistrationDAO();
+            //2.2 Call method of DAO object
+            boolean result = dao.deleteAccount(username);
+
+            //3. Process
+            if (result) {
+                //refresh --> call previous function again
+                //--> remind --> add request pars into url based on how many input controls
+                url = "DispatchServlet"
+                        + "?btAction=Search"
+                        + "&txtSearchValue=" + searchValue;
+            }//delete success
+        } catch (SQLException ex) {
+            log("SQL: " + ex.getMessage());
+        } catch (ClassNotFoundException ex) {
+            log("Class Not Found: " + ex.getMessage());
+        } finally {
+            //ko xai forward vi co 2 btaction
+            response.sendRedirect(url);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
